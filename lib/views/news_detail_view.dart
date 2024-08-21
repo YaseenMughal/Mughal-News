012212d6bb8501832baf6d15b7f76852e3eea702
Yaseen_Ismail <dev.yaseenismail@gmail.com>
2024-08-22 02:00:06 +0500

    Mughal News by Mughal-Tech

--- lib/views/news_detail_view.dart
+++ lib/views/news_detail_view.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:intl/intl.dart';
 import 'package:mughal_news/res/constant/color_constant.dart';
